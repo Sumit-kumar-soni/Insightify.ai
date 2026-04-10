f9cbb367b1a027bee38dd9b7af75626018fac588
Sumit Kumar Soni <Sumitsoni951123@gmail.com>
2026-04-10 20:33:57 +0530

    Update data_loader.py

--- Insightify.ai/modules/data_loader.py
+++ Insightify.ai/modules/data_loader.py
@@ -34,7 +34,8 @@ def load_file(file_obj):
                 df = pd.read_csv(file_obj, sep=None, engine='python')
                 
     elif filename.endswith('.xlsx'):
-        df = pd.read_excel(file_obj)
+        file_obj.seek(0)
+        df = pd.read_excel(file_obj, engine='openpyxl')
         
     elif filename.endswith('.json'):
         df = pd.read_json(file_obj)
